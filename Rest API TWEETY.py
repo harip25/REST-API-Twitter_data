a627dffe5019de2f0ab42cbc146d55f6840df05f
Hari Prasad Acharya <harip4368@gmail.com>
2020-08-15 22:47:38 +0530

    Add files via upload

---
+++ Rest API TWEETY.py
@@ -0,0 +1,101 @@
+
+#Using the REST API to collect historical data
+import tweepy  
+import time
+
+#use your own key
+access_token = "1226150405844791296-yn8Q28RBr2NLPyAuxsX8nQKO4zXDf4"  
+access_token_secret = "HAa7VPfYMfN5NyZvJNi6BFgYKMwHq5kCSpiUH31dYOd6j"  
+consumer_key = "hLmnDyuYZlQH2pet4M6by8rGW"  
+consumer_secret = "H9QEh5ZhX0sTq9dNjr0ZOtuwyAkdNHdvb9Jxe1oqlcbDYkamlS"  
+ 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
+auth.set_access_token(access_token, access_token_secret)  
+api = tweepy.API(auth,wait_on_rate_limit= True)
+
+#Obtaining Twitter’s Id with the screen name and vice versa
+user = api.get_user(screen_name = 'theresa_may')  
+print(user.id)
+
+user = api.get_user(747807250819981312)  
+print(user.screen_name)
+
+
+#Gathering the tweets from the Timeline of a certain user
+try:  
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name="theresa_may", exclude_replies=True, count = 10).items():  
+                    tweet_text = tweet.text  
+                    time = tweet.created_at  
+                    tweeter = tweet.user.screen_name  
+                    print("Text:" + tweet_text + ", Timestamp:" + str(time) + ", user:" +  tweeter)  
+except tweepy.TweepError:  
+    time.sleep(60)
+
+
+import json    
+try:  
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name="theresa_may", exclude_replies=True, count = 10).items():  
+                    tweet_text = tweet.text  
+                    time = tweet.created_at  
+                    tweeter = tweet.user.screen_name  
+                    tweet_dict = {"tweet_text" : tweet_text.strip(), "timestamp" : str(time), "user" :tweeter}  
+                    tweet_json = json.dumps(tweet_dict)  
+                    print(tweet_json)  
+except tweepy.TweepError:  
+    time.sleep(60)
+
+#Gathering the followers of a certain user.
+try:   
+    followers = api.followers_ids(screen_name="theresa_may")  
+except tweepy.TweepError:  
+    time.sleep(20)
+
+
+user_list = ["AaltoUniversity", "helsinkiuni","HAAGAHELIAamk", "AaltoENG"]
+follower_list = []  
+for user in user_list:  
+    try:   
+        followers = api.followers_ids(screen_name=user)  
+    except tweepy.TweepError:  
+        time.sleep(20)  
+        continue  
+    follower_list.append(followers)
+
+for index, user in enumerate(user_list):  
+    print("User: " + user + "\t Number of followers: " + str(len(follower_list[index])))
+
+#--------------another way-----------------------
+user_list = ["AaltoUniversity", "helsinkiuni","HAAGAHELIAamk", "AaltoENG"]    
+follower_list = []  
+for user in user_list: 
+    followers = []  
+    try:         
+        for page in tweepy.Cursor(api.followers_ids, screen_name=user).pages():  
+            followers.extend(page)  
+    except tweepy.TweepError:  
+        time.sleep(20)  
+        continue  
+    follower_list.append(followers)
+
+for index, user in enumerate(user_list):  
+    print("User: " + user + "\t Number of followers: " + str(len(follower_list[index])))
+
+#Collecting the friends of a certain user.
+friends = []  
+try:   
+    for page in tweepy.Cursor(api.friends_ids, screen_name="theresa_may").pages():  
+        friends.extend(page)  
+except tweepy.TweepError:  
+    time.sleep(20)
+
+#Seeing the number of followers/friends of a certain user.
+user = api.get_user(screen_name = 'theresa_may')  
+print(user.followers_count)  
+print(user.friends_count)
+
+user = api.get_user(747807250819981312)
+print(user.followers_count)  
+print(user.friends_count)
+
+
+    
